fb65ad3a6b4e85b1ddb1e33557a752e4fb40715f
luongngodac <55689857+luongngodac@users.noreply.github.com>
2020-04-15 21:32:01 +0700

    them

---
+++ app/KieuDuLieu.java
@@ -0,0 +1,50 @@
+package app;
+
+public class KieuDuLieu {
+
+
+
+
+
+
+
+
+
+    /*
+    Các biến không gì khác ngoài các vị trí bộ nhớ dành riêng để lưu các giá trị.
+    Nghĩa là, khi bạn tạo một biến, gbanj dự trữ một số không gian trong bộ nhớ.
+    Dựa trên kiểu dữ liệu của một biến, hệ điều hành cấp phát bộ nhớ và quyết định cái gì 
+    có thể được lưu giữ trong bộ nhớ dành riêng. Vì thế, bằng việc gán các kiểu dữ liệu khác nhau tới các biến, 
+    bạn có thể lưu giữ integer, thập phân, hoặc các ký tự trong những biến này.
+    Có hai kiểu dữ liệu có sẵn trong Java:
+    Các kiểu dữ liệu gốc (Primitive)
+    Các kiểu dữ liệu tham chiếu/ đối tượng (không phải là kiểu gốc Non-primitive)
+    boolean; char , byte, short, int, long, float, double.
+    Các kiểu dữ liệu gốc trong Java: 
+    char: Dùng để lưu trữ dữ liệu kiểu kí tự hoặc số nguyên không âm có kích thước 2 byte(16 bit)
+    Giá trị nhỏ nhất là '\u0000' (hoặc 0) và giá trị lớn nhất là 'uffff' (hoặc 65,535)..
+    Ví dụ: char nam = 'T'
+    Tại sao java sử dụng Unicode?
+    Trước Unicode, có nhiều chuẩn ngôn ngũ như ASCII, ISO 8859-1, KOI-8,
+    GB18030, ... Điều này gây là hai vấn đề sau:
+    Một giá trị code cụ thể tương ứng với các chữ khác nhau trong các chuẩn ngôn ngữ đa dạng.
+    Mã hóa cho các ngôn ngữ với các tập ký tự lớn có độ dài biến đổi. Một số ký tự 
+    chung được mã hóa thành các byte đơn, một số khác cần hai hoặc nhiều byte.
+    Để xử lý các ván đề này, một chuẩn ngôn ngữ mới được phát triển, đó là 
+    Unicode. Trong Unicode, ký tự giữ 2 byte, vì thế java cũng sử dụng 2 byte cho các ký tự.
+    Kiểu dữ liệu byte: dùng  để lưu dữ liệu kiểu số nguyên có kích thước một byte (8bit)
+    Kiểu dữ liệu short:Dùng để lưu dữ liệu có kiểu số nguyên, kích cỡ 2 byte(16 bit)
+    int: Dùng để lưu trữ dữ liệu có kiểu số nguyên, kích cỡ 4 byte (4 bit).
+    long: Dùng để lưu trữ dữ liệu có kiểu số nguyên có kích thước lên đến 8 byte.
+    float: Dùng để lưu dữ liệu có kiểu số thực, kích cỡ 4 byte(32 bit)
+    double: kiểu dữ liệu double được sử dụng để lưu trữ dữ liệu có kiểu số thực, kích thước 8 byte.
+    boolean: độ lớn chỉ 1 bit dùng để lưu trữ dữ liệu chỉ có hai trạng thái true hoặc false. Giá trị mặc định là flase.
+    Kiểu dữ liệu Object trong Java: 
+    Các biến đối tượng được tạo bởi sử dụng các constructor đã được định nghãi của các lớp.
+    Literal trong Java:
+    
+    
+    
+    
+    VietJack.com*/
+}

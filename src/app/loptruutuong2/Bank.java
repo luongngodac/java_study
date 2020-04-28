5be96390eeb427bb96730fa5559e4f47136e9472
luongngodac <55689857+luongngodac@users.noreply.github.com>
2020-04-28 23:08:12 +0700

    lop truu tuong trong java

---
+++ src/app/loptruutuong2/Bank.java
@@ -0,0 +1,6 @@
+package app.loptruutuong2;
+
+abstract class Bank
+    {
+        abstract int getRateOfInterest();
+    }
---
+++ src/app/loptruutuong2/TestBank.java
@@ -0,0 +1,13 @@
+package app.loptruutuong2;
+
+class TestBank
+{
+    public static void main(String args[])
+    {
+        //Tao mot doi tuong SBI moi.
+        Bank b = new SBI();
+        //Neu doi tuong la PNB, phuong thuc cua PNB se duoc trieu hoi.
+        int interest = b.getRateOfInterest();//Trieu hoi phuong thuc cua SBI.
+        System.out.println("ti le lai suat la: " + interest + " %");
+    }
+}

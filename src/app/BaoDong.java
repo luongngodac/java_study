cc24bd82d28c748d95320bb31b7a140d698a66e0
luongngodac <55689857+luongngodac@users.noreply.github.com>
2020-05-18 11:55:29 +0700

    tinh bao dong trong java

---
+++ src/app/BaoDong.java
@@ -0,0 +1,30 @@
+package app;
+
+public class BaoDong {
+    /*
+    Tính bao đóng trong Java
+    Tính bao đóng trong Java là một tiến trình đóng gói code và dữ liệu lại với nhau trong một đơn vị unit đơn, 
+    ví dụ như một gói bột giặt là hỗn hợp của các hạt bột giặt khác nhau. Chúng ta có thể tạo một lớp được bao 
+    đóng hoàn toàn trong java bằng việc tạo tất cả thành viên dữ liệu của lớp là private.
+    Bây giờ, chúng ta sử dụng phương thức seter và getter để theiets để thiết lập và lấy dữ liệu trong nó. 
+    Lớp Java Bean là ví dụ về một lớp được bao đóng hoàn toàn.
+    Tính bao đóng là kỹ thuật tạo một trường của lớp private và cung cấp khả năng truy cập trường này qua các phương thức public.
+    Nếu một trường được khai báo private, nó không thể được truy cập bởi bên ngoài lớp, do đó có thể che dấu các trường có lớp này.
+    Vì lý do này, tính bao đóng được ám chỉ như việc dấu dữ liệu (datat hiding).
+    Tính bao đóng có thể được môt at như là một tấm báo vệ code và tránh code và dữ liệu của bạn bị truy cập một cách ngẫu nhiên bởi các code khác bên ngoài class.
+    Truy cập dữ liệu và code được điều khiển một cách chặt ché bởi interface.
+    
+    Lợi thế của tính bao đóng trong Java.
+    Bởi cung cấp phương thức setter hoặc gettter, bạn có thế làm cho lớp là read-only hoặc writer-only.
+    Nó cung cấp cho bạn sự điều khiển thông qua dữ liệu. Giả sử bạn muốn thiết lập giá trị của id là lớn hơn 100,thì bạn có thể viết biểu thức logic bên trong phương thức setter.
+    Tất cả các lớp có thể có chế độ chỉ đọc hoặc chỉ ghi (chỉ có hàm getter hoặc setter)
+    Một lớp có thể toàn bộ điều khiển thông qua những gì được lưu trữ trong các trường của nó.
+    Người sử dụng của clas không biết cách các class lưu trữ dữ liệu Một class có thể thay đổi 
+    kiểu dữ liệu của một trường với phương thức settter và getter của nó.
+    Chạy trong package baodong.
+    gồm hai lớp: Student.java và Test.java
+    
+    vietJack.com
+    */
+    
+}
